0dfa558ae06194bc98b5b62631ca04ac706402c4
Mert Oyman <mertoyman@gmail.com>
2016-10-20 17:05:39 +0300

    Hand_Prefab_Scripts&LeverController Script added

---
+++ Flood Simulation/Assets/Scripts/AttachHand.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachHand : MonoBehaviour {
+
+	public Transform controller;
+	//private bool hammerIsAttached = false;
+
+	// Use this for initialization
+	void Start () {
+        transform.localPosition = new Vector3(0, 3.42f, -14.85f);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       // transform.localPosition = new Vector3(0, -1.33f, -5.83f);
+        transform.rotation	= controller.rotation;
+       // transform.SetParent(controller);
+        transform.Rotate(0,0,0);
+        //hammerIsAttached = true;
+    }
+
+}
