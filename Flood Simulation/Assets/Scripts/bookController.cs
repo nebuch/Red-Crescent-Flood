9d908b19d05bfe045255628fd809e9aaebc68d46
Nebuch <dogancihad@gmail.com>
2016-11-07 19:38:00 +0200

    intro final

---
+++ Flood Simulation/Assets/Scripts/bookController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bookController : MonoBehaviour {
+
+	public int _bookType;
+
+	public Collider _bookCover;
+
+	public bool _bookOpen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void BookOpen(){
+	
+		_bookOpen = true;
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+	
+		if (other == _bookCover) {
+		
+			BookOpen ();
+
+		}
+	
+	}
+}
